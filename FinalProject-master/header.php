ed50fdedefbbd2557bba61e4254acd4f2cff38ce
Ratra Pongwanich <kaow_ps3@hotmail.com>
2017-02-13 22:25:51 +0700

    p

---
+++ FinalProject-master/header.php
@@ -0,0 +1,48 @@
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="css/video.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#"><img src="img/logo.jpg" alt="nextlevel.logo" height="35" width="150"></a>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Products
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">New Releases</a>
+          <a class="dropdown-item" href="#">Console</a>
+          <a class="dropdown-item" href="#">Games</a>
+          <a class="dropdown-item" href="#">Toys</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Payment</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Review Game</a>
+      </li>
+    </ul>
+  </div>
+  <ul class="nav navbar-nav pull-sm-right">
+                <li class="nav-item">
+                    <a class="btn btn-outline btn-block" href="#">Sign in</a>
+                </li>
+            </ul>
+</nav>
---
+++ FinalProject-master/review.php
@@ -0,0 +1,65 @@
+<?php include("header.php") ?>
+<!-- Type your code here -->
+
+    
+
+    <section class="jumbotron text-center">
+      <div class="container">
+        <h1 class="jumbotron-heading">Game Review</h1>
+        <p class="lead text-muted">Here we have the lastest games </p>
+      </div>
+    </section>
+    <!--game review-->
+    <section id="feature-one">
+        <div class="container">
+            <div class="row">
+                    <div class="col-md-6">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/j1kh8M_BjEE" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    <div class="col-md-6">
+                        <h6>Watch this</h6>
+                        <h2>Stardew Valley </h2>
+                        <p class="lead">Stardew Valley is an RPG-style farming game. It's up to you to choose your path... Will you devote your time to raising crops and caring for animals? Will you spend time in the local village, befriending the local townspeople? Do you get a kick out of slaying dreadful monsters in the mysterious mountain caverns? Or will you try to accomplish everything and claim the coveted title of Stardew Hero?</p>
+                    </div>
+            </div>
+        </div>
+    </section>
+    <br />
+     <section class="jumbotron">
+        <div class="container">
+            <div class="row">
+
+                    <div class="col-md-6">
+                        <h6>Watch this</h6>
+                        <h2>NIOH</h2>
+                        <p class="lead">Take up your sword and travel to Japans blood-bathed Sengoku period an era ravaged by warring states and dark, malevolent forces and cut a violent path through the land as the masterless samurai, William. Cross blades in brutal hand-to-hand combat, wielding swords, axes, spears and even war hammers against foes both human and demon. Endure the vicious encounters and learn from your mistakes: each death will bring you resurrection and each resurrection a greater resolve to overcome your foes.</p>
+                    </div>
+                    <div class="col-md-6">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/qZg6kMZbAzU" frameborder="0" allowfullscreen></iframe>
+                    </div>
+
+                    
+            </div>
+        </div>
+    </section>
+    <br />
+     <section id="feature-one">
+        <div class="container">
+            <div class="row">
+                    <div class="col-md-6">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/9U0MQg_mUeU" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    <div class="col-md-6">
+                        <h6>Watch this</h6>
+                        <h2>Gears of War 4</h2>
+                        <p class="lead">A new saga begins. After narrowly escaping an attack on their village, JD Fenix and his friends, Kait and Del, must rescue the ones they love and discover the source of a monstrous new enemy. Engage in a two-player co-op campaign with friends locally via split-screen, over Xbox Live or LAN. Player 2 can select either Kait or Del. </p>
+                    </div>
+            </div>
+        </div>
+    </section>
+    <br />
+    <br />
+
+    
+
+<?php include("footer.php") ?>
